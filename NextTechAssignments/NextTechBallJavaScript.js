ac6ef5ad2a9cc7c73b9de984a8957dd18b3171fc
Danny Rivera <99273137+OhhNoo2150@users.noreply.github.com>
2022-03-03 09:16:17 -0600

    Update NextTechBallJavaScript.js
    defined the moveBall function. it was lacking

--- NextTechAssignments/NextTechBallJavaScript.js
+++ NextTechAssignments/NextTechBallJavaScript.js
@@ -1,11 +1,17 @@
 //Set global variable that would contain the position, velocity and the html element "ball"
-  var positionX = 0;
-  var positionY = 0;
-  var velocity = 100;
-  var reverse = false;
-  var ball = document.getElementById("ball");
+var positionX = 0;
+var positionY = 0;
+var velocity = 100;
+var reverse = false;
+var ball = document.getElementById("ball");
+
+//write a function that can change the position of the html element "ball"
+function moveBall() {
+  // two x-axis coordinates
   var Xmin = 0;
   var Xmax = 300;
+
+  // two y-axis coordinates
   var Ymin = 0;
   var Ymax = 300;
 
