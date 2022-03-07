fe1bdbb81498d5f0e1729416472e255e1895c9cd
Daniel Rivera <daniel.i.rivera@protonmail.com>
2022-03-07 09:26:34 -0600

    next tech assignments and activities

---
+++ NextTechAssignments/NextTechFatArrowFunctions.js
@@ -0,0 +1,27 @@
+function addFive(num) {
+    return num + 5;
+  }
+  let addFive = (num) => {
+    num + 5;
+  }; 
+  
+  
+  
+  function divideBy(x, y) {
+    return x / y;
+  }
+  let divideBy = (x, y) => {
+    return x / y;
+  };
+  
+  
+  
+  function logToConsole() {
+    let msg = "MIT Coding Certificate";
+    console.log(msg);
+  }
+  let logToConsole = () => {
+    let msg = "MIT Coding Certificate";
+    console.log(msg);
+  };
+  
---
+++ NextTechAssignments/NextTechFirstArrayTask02.js
@@ -0,0 +1,25 @@
+//Instructions 
+//Given an array of strings, words, return a string that has all the words concatenated together
+// - spaces are needed in between words
+//
+//ex. ['how', 'are', 'you'] -> 'how are you'
+
+function wordsToSentence(words) {
+    var sentence = ''; //your code here
+
+    for (let i = 0; i < words.length; i++){
+        sentence += words[i] + ' ';
+    }
+    return sentence.trim();
+}
+    
+  //console log results
+  console.log('results', wordsToSentence(['hey', 'there']));
+  
+  //don't change this line
+  if (typeof module !== 'undefined') {
+    module.exports = {
+      wordsToSentence,
+    };
+  }
+  
